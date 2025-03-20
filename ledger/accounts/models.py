ed380a899c5105003d94b5bc3816e91263625cc8
Priya Thakur <thakurpriya1990@gmail.com>
2025-03-20 16:43:51 +0800

    resolve the update address oscar_address-id issue

--- ledger/accounts/models.py
+++ ledger/accounts/models.py
@@ -22,6 +22,7 @@ from django_countries.fields import CountryField
 from datetime import datetime, date
 
 from ledger.accounts.signals import name_changed, post_clean
+from django.db.models.signals import post_delete, pre_save, post_save
 from ledger.accounts.utils import get_department_user_compact, in_dbca_domain
 from ledger.address.models import UserAddress, Country
 from django.conf import settings
@@ -583,3 +584,158 @@ class Profile(RevisionedMixin):
             return '{} ({})'.format(self.name, self.email)
         else:
             return '{}'.format(self.email)
+
+class ProfileListener(object):
+    """
+    Event listener for Profile
+
+    """
+    @staticmethod
+    @receiver(post_delete, sender=Profile)
+    def _post_delete(sender, instance, **kwargs):
+        # delete from email identity, and social auth
+        if instance.user.email == instance.email:
+            # profile's email is user's email, return
+            return
+
+        # delete the profile's email from email identity and social auth
+        EmailIdentity.objects.filter(email=instance.email, user=instance.user).delete()
+        UserSocialAuth.objects.filter(provider="email", uid=instance.email, user=instance.user).delete()
+
+    @staticmethod
+    @receiver(pre_save, sender=Profile)
+    def _pre_save(sender, instance, **kwargs):
+        if not hasattr(instance, "auth_identity"):
+            # not triggered by user.
+            return
+
+        if instance.pk:
+            original_instance = Profile.objects.get(pk=instance.pk)
+            setattr(instance, "_original_instance", original_instance)
+        elif hasattr(instance, "_original_instance"):
+            delattr(instance, "_original_instance")
+
+    @staticmethod
+    @receiver(post_save, sender=Profile)
+    def _post_save(sender, instance, **kwargs):
+        if not hasattr(instance, "auth_identity"):
+            # not triggered by user.
+            return
+
+        original_instance = getattr(instance, "_original_instance") if hasattr(instance, "_original_instance") else None
+        auth_identity = getattr(instance, "auth_identity")
+        if auth_identity:
+            # add email to email identity and social auth if not exist
+            EmailIdentity.objects.get_or_create(email=instance.email, user=instance.user)
+            if not UserSocialAuth.objects.filter(user=instance.user, provider="email", uid=instance.email).exists():
+                user_social_auth = UserSocialAuth.create_social_auth(instance.user, instance.email, 'email')
+                user_social_auth.extra_data = {'email': [instance.email]}
+                user_social_auth.save()
+
+        if original_instance and (original_instance.email != instance.email or not auth_identity):
+            # delete the profile's email from email identity and social auth
+            EmailIdentity.objects.filter(email=original_instance.email, user=original_instance.user).delete()
+            UserSocialAuth.objects.filter(provider="email", uid=original_instance.email, user=original_instance.user).delete()
+
+        if not original_instance:
+            address = instance.postal_address
+            try:
+                # Check if the user has the same profile address
+                # Check if there is a user address
+                oscar_add = UserAddress.objects.get(
+                    line1 = address.line1,
+                    line2 = address.line2,
+                    line3 = address.line3,
+                    line4 = address.locality,
+                    state = address.state,
+                    postcode = address.postcode,
+                    country = Country.objects.get(iso_3166_1_a2=address.country),
+                    user = instance.user
+                )
+                if not address.oscar_address:
+                    address.oscar_address = oscar_add
+                    address.save()
+                elif address.oscar_address.id != oscar_add.id:
+                    address.oscar_address = oscar_add
+                    address.save()
+            except UserAddress.DoesNotExist:
+                oscar_address = UserAddress.objects.create(
+                    line1 = address.line1,
+                    line2 = address.line2,
+                    line3 = address.line3,
+                    line4 = address.locality,
+                    state = address.state,
+                    postcode = address.postcode,
+                    country = Country.objects.get(iso_3166_1_a2=address.country),
+                    user = instance.user
+                )
+                address.oscar_address = oscar_address
+                address.save()
+        # Clear out unused addresses
+        # EmailUser can have address that is not linked with profile, hence the exclude
+        ''' This functionality no longer in use due to more than just
+        profile objects using the UserAddresses
+        user = instance.user
+        user_addr = Address.objects.filter(user=user)
+        for u in user_addr:
+            if not u.profiles.all() \
+                and not u in (user.postal_address, user.residential_address, user.billing_address):
+                u.oscar_address.delete()
+                u.delete()'''
+
+class AddressListener(object):
+    """
+        Event listener for Address
+    """
+    @staticmethod
+    @receiver(pre_save, sender=Address)
+    def _pre_save(sender, instance, **kwargs):
+        check_address = UserAddress(
+            line1 = instance.line1,
+            line2 = instance.line2,
+            line3 = instance.line3,
+            line4 = instance.locality,
+            state = instance.state,
+            postcode = instance.postcode,
+            country = Country.objects.get(iso_3166_1_a2=instance.country),
+            user = instance.user
+        )
+        if instance.pk:
+            original_instance = Address.objects.get(pk=instance.pk)
+            setattr(instance, "_original_instance", original_instance)
+            if original_instance.oscar_address is None:
+                try:
+                    check_address = UserAddress.objects.get(hash=check_address.generate_hash(),user=check_address.user)
+                except UserAddress.DoesNotExist:
+                    check_address.save()
+                instance.oscar_address = check_address
+        elif hasattr(instance, "_original_instance"):
+            delattr(instance, "_original_instance")
+        else:
+            try:
+                check_address = UserAddress.objects.get(hash=check_address.generate_hash(),user=check_address.user)
+            except UserAddress.DoesNotExist:
+                check_address.save()
+            instance.oscar_address = check_address
+
+    @staticmethod
+    @receiver(post_save, sender=Address)
+    def _post_save(sender, instance, **kwargs):
+        original_instance = getattr(instance, "_original_instance") if hasattr(instance, "_original_instance") else None
+        if original_instance:
+            oscar_address = original_instance.oscar_address
+            try:
+                if oscar_address is not None:
+                    oscar_address.line1 = instance.line1
+                    oscar_address.line2 = instance.line2
+                    oscar_address.line3 = instance.line3
+                    oscar_address.line4 = instance.locality
+                    oscar_address.state = instance.state
+                    oscar_address.postcode = instance.postcode
+                    oscar_address.country = Country.objects.get(iso_3166_1_a2=instance.country)
+                    oscar_address.save()
+            except IntegrityError as e:
+                if 'unique constraint' in e.message:
+                    raise ValidationError('Multiple profiles cannot have the same address.')
+                else:
+                    raise
--- ledger/oscar_fields.py
+++ ledger/oscar_fields.py
@@ -20,26 +20,49 @@ class Creator(object):
         obj.__dict__[self.field.name] = self.field.to_python(value)
 
 
+# class UppercaseCharField(CharField):
+#     """
+#     A simple subclass of ``django.db.models.fields.CharField`` that
+#     restricts all text to be uppercase.
+
+#     Defined with the with_metaclass helper so that to_python is called
+#     https://docs.djangoproject.com/en/1.6/howto/custom-model-fields/#the-subfieldbase-metaclass  # NOQA
+#     """
+
+#     def contribute_to_class(self, cls, name, **kwargs):
+#         super(UppercaseCharField, self).contribute_to_class(
+#             cls, name, **kwargs)
+#         setattr(cls, self.name, Creator(self))
+
+#     def from_db_value(self, value, expression, connection, context):
+#         return self.to_python(value)
+
+#     def to_python(self, value):
+#         val = super(UppercaseCharField, self).to_python(value)
+#         if isinstance(val, six.string_types):
+#             return val.upper()
+#         else:
+#             return val
+
 class UppercaseCharField(CharField):
     """
     A simple subclass of ``django.db.models.fields.CharField`` that
     restricts all text to be uppercase.
-
-    Defined with the with_metaclass helper so that to_python is called
-    https://docs.djangoproject.com/en/1.6/howto/custom-model-fields/#the-subfieldbase-metaclass  # NOQA
     """
 
-    def contribute_to_class(self, cls, name, **kwargs):
-        super(UppercaseCharField, self).contribute_to_class(
-            cls, name, **kwargs)
+    def contribute_to_class(self, cls, name, private_only=False, **kwargs):
+        super().contribute_to_class(cls, name, private_only=private_only, **kwargs)
         setattr(cls, self.name, Creator(self))
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, *args, **kwargs):
         return self.to_python(value)
 
     def to_python(self, value):
-        val = super(UppercaseCharField, self).to_python(value)
-        if isinstance(val, six.string_types):
+        """
+        Cast the supplied value to uppercase
+        """
+        val = super().to_python(value)
+        if isinstance(val, str):
             return val.upper()
         else:
             return val
