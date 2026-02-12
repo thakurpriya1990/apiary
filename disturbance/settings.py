73b23ac6fb44097b55418b0cecec970ccb52a7b6
Ash <ash.woodiss-field@dbca.gov.wa.au>
2026-02-12 10:43:46 +0800

    Added: default pagination, cleaned up approval api

--- disturbance/settings.py
+++ disturbance/settings.py
@@ -59,6 +59,8 @@ REST_FRAMEWORK = {
     ),
     "EXCEPTION_HANDLER": "disturbance.exceptions.custom_exception_handler",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 USE_DJANGO_JQUERY= True
