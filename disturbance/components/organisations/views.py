c30a1ae3569748c5681b4572edb231f57b7b8d3d
Ash <ash.woodiss-field@dbca.wa.gov.au>
2026-02-24 16:42:16 +0800

    Update: drafted solution for securing history compare views, but said views do not currently work

--- disturbance/components/organisations/views.py
+++ disturbance/components/organisations/views.py
@@ -1,10 +1,10 @@
 from disturbance.components.organisations.models import Organisation
 from reversion_compare.views import HistoryCompareDetailView
 
-
-class OrganisationHistoryCompareView(HistoryCompareDetailView):
-    """
-    View for reversion_compare
-    """
-    model = Organisation
-    template_name = 'disturbance/reversion_history.html'
+#TODO fix for segregation (fix or remove all history compare views (they are not secured!))
+#class OrganisationHistoryCompareView(HistoryCompareDetailView):
+#    """
+#    View for reversion_compare
+#    """
+#    model = Organisation
+#    template_name = 'disturbance/reversion_history.html'
