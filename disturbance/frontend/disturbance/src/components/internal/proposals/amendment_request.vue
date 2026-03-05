463f81fe5a12c36884cd01ddd51cb541c12a0c6d
Ash <ash.woodiss-field@dbca.wa.gov.au>
2026-03-05 15:48:04 +0800

    Update: client side validation will now highlight missing required fields

--- disturbance/frontend/disturbance/src/components/internal/proposals/amendment_request.vue
+++ disturbance/frontend/disturbance/src/components/internal/proposals/amendment_request.vue
@@ -107,7 +107,11 @@ export default {
         ok:function () {
             let vm =this;
             if($(vm.form).valid()){
+                vm.errors = false;
                 vm.sendData();
+            } else {
+                vm.errorString = "Missing required fields.";
+                vm.errors = true;
             }
         },
         cancel:function () {
@@ -200,31 +204,7 @@ export default {
             vm.validation_form = $(vm.form).validate({
                 rules: {
                     reason: "required"
-
-
                 },
-                messages: {
-                    reason: "field is required",
-
-                },
-                showErrors: function(errorMap, errorList) {
-                    $.each(this.validElements(), function(index, element) {
-                        var $element = $(element);
-                        $element.attr("data-original-title", "").parents('.form-group').removeClass('has-error');
-                    });
-                    // destroy tooltips on valid elements
-                    $("." + this.settings.validClass).tooltip("destroy");
-                    // add or update tooltips
-                    for (var i = 0; i < errorList.length; i++) {
-                        var error = errorList[i];
-                        $(error.element)
-                            .tooltip({
-                                trigger: "focus"
-                            })
-                            .attr("data-original-title", error.message)
-                            .parents('.form-group').addClass('has-error');
-                    }
-                }
             });
        },
        eventListeners:function () {
