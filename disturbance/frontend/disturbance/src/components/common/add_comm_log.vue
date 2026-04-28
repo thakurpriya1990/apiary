57d8b98ee144d42391b3d55de040f7cc243cad38
shibaken@gmail.com <shibaken@gmail.com>
2026-04-28 09:05:28 +0800

    Style: update button classes to primary for better visibility in communication log

--- disturbance/frontend/disturbance/src/components/common/add_comm_log.vue
+++ disturbance/frontend/disturbance/src/components/common/add_comm_log.vue
@@ -94,8 +94,8 @@
                 </div>
             </div>
             <template #footer>
-                <button type="button" v-if="addingComms" disabled class="btn btn-default" @click="ok"><i class="fa fa-spinner fa-spin"></i> Adding</button>
-                <button type="button" v-else class="btn btn-default" @click="ok">Add</button>
+                <button type="button" v-if="addingComms" disabled class="btn btn-primary" @click="ok"><i class="fa fa-spinner fa-spin"></i> Adding</button>
+                <button type="button" v-else class="btn btn-primary" @click="ok">Add</button>
                 <button type="button" class="btn btn-default" @click="cancel">Cancel</button>
             </template>
         </modal>
