7848fc58ef10c2e9ff24d4aa7e968ece307df491
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-08-03 13:37:10 +0800

    Tidy and fix minor bug

--- disturbance/management/commands/cron_tasks.py
+++ disturbance/management/commands/cron_tasks.py
@@ -15,52 +15,33 @@ class Command(BaseCommand):
     help = "Run the Disturbance Cron tasks"
 
     def handle(self, *args, **options):
-        stdout_redirect = " | tee -a {}".format(LOGFILE)
-        subprocess.call(
-            "cat /dev/null > {}".format(LOGFILE), shell=True
-        )  # empty the log file
+        stdout_redirect = f" | tee -a {LOGFILE}"
+        subprocess.call(f"cat /dev/null > {LOGFILE}", shell=True)  # empty the log file
 
-        logger.info("Running command {}".format(__name__))
-        subprocess.call(
-            "python manage.py update_compliance_status" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage.py send_compliance_reminder" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage.py update_approval_status" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage.py expire_approvals" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage.py approval_renewal_notices" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage.py send_assessment_reminder" + stdout_redirect, shell=True
-        )
-        subprocess.call(
-            "python manage.py weekly_assessment_reminder" + stdout_redirect, shell=True
-        )
+        logger.info(f"Running command {__name__}")
+        subprocess.call("python manage.py update_compliance_status" + stdout_redirect, shell=True)
+        subprocess.call("python manage.py send_compliance_reminder" + stdout_redirect, shell=True)
+        subprocess.call("python manage.py update_approval_status" + stdout_redirect, shell=True)
+        subprocess.call("python manage.py expire_approvals" + stdout_redirect, shell=True)
+        subprocess.call("python manage.py approval_renewal_notices" + stdout_redirect, shell=True)
+        subprocess.call("python manage.py send_assessment_reminder" + stdout_redirect, shell=True)
+        subprocess.call("python manage.py weekly_assessment_reminder" + stdout_redirect, shell=True)
         subprocess.call(
             "python manage.py send_annual_rental_fee_invoice" + stdout_redirect,
             shell=True,
         )
-        subprocess.call(
-            "python manage.py save_apiary_sites" + stdout_redirect, shell=True
-        )
+        subprocess.call("python manage.py save_apiary_sites" + stdout_redirect, shell=True)
 
-        logger.info("Command {} completed".format(__name__))
+        logger.info(f"Command {__name__} completed")
         self.send_email()
 
     def send_email(self):
         log_txt = Path(LOGFILE).read_text()
-        # subject = '{} - Cronjob'.format(settings.SYSTEM_NAME_SHORT)
-        subject = "DAS-Apiary - Cronjob"
+        subject = "Apiary - Cronjob"
         body = ""
         to = (
             settings.CRON_NOTIFICATION_EMAIL
-            if isinstance(settings.NOTIFICATION_EMAIL, list)
+            if isinstance(settings.CRON_NOTIFICATION_EMAIL, list)
             else [settings.CRON_NOTIFICATION_EMAIL]
         )
         send_mail(
