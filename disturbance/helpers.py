69d9e5c8604e56705744f28f8321f1b988643fa0
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-08-13 16:54:25 +0800

    Add get_cached_ledger_organisation which will cache for the duration of the request

--- disturbance/helpers.py
+++ disturbance/helpers.py
@@ -1,18 +1,24 @@
-from __future__ import unicode_literals
-from ledger_api_client.ledger_models import EmailUserRO as EmailUser, UsersInGroup
+import logging
+from functools import lru_cache
+
 from django.conf import settings
 from django.contrib.auth.models import Group
-
-import logging
-
+from django.core.cache import cache
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+from ledger_api_client.ledger_models import UsersInGroup
+from ledger_api_client.utils import get_organisation
 from rest_framework import serializers
 
-from disturbance.components.organisations.models import ApiaryOrganisationAccessGroupMember
 from disturbance.components.main.models import MapLayer
-from disturbance.components.proposals.models import ApiaryAssessorGroupMember, ApiaryApproverGroupMember, ApiaryReferralGroupMember 
-from django.core.cache import cache
+from disturbance.components.organisations.models import ApiaryOrganisationAccessGroupMember
+from disturbance.components.proposals.models import (
+    ApiaryApproverGroupMember,
+    ApiaryAssessorGroupMember,
+    ApiaryReferralGroupMember,
+)
 
 logger = logging.getLogger(__name__)
+
 
 def belongs_to(user, group_name):
     """
@@ -23,50 +29,89 @@ def belongs_to(user, group_name):
     """
     group = Group.objects.filter(name=group_name)
     if group.exists():
-        return user.id in list(UsersInGroup.objects.filter(group_id=group.first().id).values_list('emailuser_id', flat=True))
+        return user.id in list(
+            UsersInGroup.objects.filter(group_id=group.first().id).values_list("emailuser_id", flat=True)
+        )
     else:
         return False
 
-#NOTE: this is an env settings based group
+
+# NOTE: this is an env settings based group
 def is_apiary_admin(request):
     return request.user.is_authenticated and (belongs_to(request.user, settings.APIARY_ADMIN_GROUP))
 
-def is_in_organisation_contacts(request, organisation):
-    return request.user.email in organisation.contacts.all().values_list('email', flat=True)
 
-#NOTE: this is an env settings based group - consider renaming or replacing to avoid confusion
+def is_in_organisation_contacts(request, organisation):
+    return request.user.email in organisation.contacts.all().values_list("email", flat=True)
+
+
+# NOTE: this is an env settings based group - consider renaming or replacing to avoid confusion
 def is_approved_external_user(request):
     if belongs_to(request.user, settings.APPROVED_APIARY_EXTERNAL_USERS_GROUP):
         return True
     return False
 
+
 def is_apiary_approver(request):
-    return request and request.user and (ApiaryApproverGroupMember.objects.filter(emailuser_id=request.user.id).exists() or request.user.is_superuser)
-
-def is_apiary_assessor(request):
-    return request and request.user and (ApiaryAssessorGroupMember.objects.filter(emailuser_id=request.user.id).exists() or request.user.is_superuser)
-
-def is_apiary_referrer(request):
-    return request and request.user and (ApiaryReferralGroupMember.objects.filter(emailuser_id=request.user.id).exists() or request.user.is_superuser)
-
-def is_apiary_org_request_assessor(request):
-    return request and request.user and (ApiaryOrganisationAccessGroupMember.objects.filter(emailuser_id=request.user.id).exists() or request.user.is_superuser)
-
-def is_internal(request):
-    return(
-        request and request.user and (
-            request.user.is_superuser or 
-            is_apiary_admin(request) or
-            is_approved_external_user(request) or
-            is_apiary_approver(request) or
-            is_apiary_assessor(request) or
-            is_apiary_referrer(request) or
-            is_apiary_org_request_assessor(request)
+    return (
+        request
+        and request.user
+        and (
+            ApiaryApproverGroupMember.objects.filter(emailuser_id=request.user.id).exists() or request.user.is_superuser
         )
     )
 
+
+def is_apiary_assessor(request):
+    return (
+        request
+        and request.user
+        and (
+            ApiaryAssessorGroupMember.objects.filter(emailuser_id=request.user.id).exists() or request.user.is_superuser
+        )
+    )
+
+
+def is_apiary_referrer(request):
+    return (
+        request
+        and request.user
+        and (
+            ApiaryReferralGroupMember.objects.filter(emailuser_id=request.user.id).exists() or request.user.is_superuser
+        )
+    )
+
+
+def is_apiary_org_request_assessor(request):
+    return (
+        request
+        and request.user
+        and (
+            ApiaryOrganisationAccessGroupMember.objects.filter(emailuser_id=request.user.id).exists()
+            or request.user.is_superuser
+        )
+    )
+
+
+def is_internal(request):
+    return (
+        request
+        and request.user
+        and (
+            request.user.is_superuser
+            or is_apiary_admin(request)
+            or is_approved_external_user(request)
+            or is_apiary_approver(request)
+            or is_apiary_assessor(request)
+            or is_apiary_referrer(request)
+            or is_apiary_org_request_assessor(request)
+        )
+    )
+
+
 def get_all_officers():
-    return EmailUser.objects.filter(groups__name='Disturbance Admin')
+    return EmailUser.objects.filter(groups__name="Disturbance Admin")
+
 
 def is_authorised_to_modify(request, instance):
     authorised = True
@@ -89,17 +134,17 @@ def is_authorised_to_modify(request, instance):
     # print('5. applicantIsIndividual', applicantIsIndividual)
     if is_internal(request):
         # the status must be 'with_assessor'
-        authorised &= instance.processing_status == 'with_assessor'
+        authorised &= instance.processing_status == "with_assessor"
         # print("6. Internal with assessor", instance.processing_status == 'with_assessor')
         # the user must be an assessor for this type of application
         authorised &= instance.can_process()
         # print('7. Can process', instance.can_process())
     else:
         # the status of the application must be DRAFT for customer to modify
-        authorised &= instance.processing_status == 'draft'
+        authorised &= instance.processing_status == "draft"
         # print('8. Processing status draft', instance.processing_status == 'draft')
         if applicantIsIndividual:
-                        # it is an individual so the applicant and submitter must be the same
+            # it is an individual so the applicant and submitter must be the same
             authorised &= str(request.user.email) == str(instance.relevant_applicant)
             # print('9. Indiv submitter matches applicant', str(request.user.email) == str(instance.relevant_applicant))
         else:
@@ -109,19 +154,20 @@ def is_authorised_to_modify(request, instance):
 
     # print('11. Authorised', authorised)
     if not authorised:
-        raise serializers.ValidationError('You are not authorised to modify this application.')
+        raise serializers.ValidationError("You are not authorised to modify this application.")
+
 
 def is_authorised_to_modify_draft(request, instance):
     authorised = True
     applicant = instance.relevant_applicant
 
     applicantIsIndividual = isinstance(applicant, EmailUser)
-    if instance.processing_status=='draft':
+    if instance.processing_status == "draft":
         if request.user and request.user.is_authenticated:
             # the status of the application must be DRAFT for customer to modify
-            #NOTE: internal users that are apiary assessors can also edit if in draft
-            if is_internal(request):   
-                authorised &= ((is_apiary_assessor(request) or request.user.is_superuser))
+            # NOTE: internal users that are apiary assessors can also edit if in draft
+            if is_internal(request):
+                authorised &= is_apiary_assessor(request) or request.user.is_superuser
             elif applicantIsIndividual:
                 # it is an individual so the applicant and submitter must be the same
                 authorised &= str(request.user.email) == str(instance.relevant_applicant)
@@ -136,34 +182,48 @@ def is_authorised_to_modify_draft(request, instance):
             # the user must be an assessor for this type of application
             authorised &= instance.can_assess()
         else:
-            authorised=False
+            authorised = False
 
     if not authorised:
-        raise serializers.ValidationError('You are not authorised to modify this application.')
-    
+        raise serializers.ValidationError("You are not authorised to modify this application.")
+
+
 def get_proxy_cache():
-    proxy_cache_dumped_data =cache.get('utils_cache.get_proxy_cache()')
+    proxy_cache_dumped_data = cache.get("utils_cache.get_proxy_cache()")
     proxy_cache_array = []
     if proxy_cache_dumped_data is None:
         proxy_cache_query = MapLayer.objects.all()
-        
-        for pr in proxy_cache_query:
-            proxy_cache_array.append({'layer_name': pr.layer_name, 'cache_expiry' : pr.cache_expiry})
 
-        cache.set('utils_cache.get_proxy_cache()', proxy_cache_array, 86400)
+        for pr in proxy_cache_query:
+            proxy_cache_array.append({"layer_name": pr.layer_name, "cache_expiry": pr.cache_expiry})
+
+        cache.set("utils_cache.get_proxy_cache()", proxy_cache_array, 86400)
     else:
-        proxy_cache_array =  proxy_cache_dumped_data
+        proxy_cache_array = proxy_cache_dumped_data
     return proxy_cache_array
 
+
 def is_internal_user(user):
-    return(
-        user and hasattr(user,'id') and (
-            user.is_superuser or
-            belongs_to(user, settings.APIARY_ADMIN_GROUP) or
-            belongs_to(user, settings.APPROVED_APIARY_EXTERNAL_USERS_GROUP) or
-            ApiaryApproverGroupMember.objects.filter(emailuser_id=user.id).exists() or
-            ApiaryAssessorGroupMember.objects.filter(emailuser_id=user.id).exists() or
-            ApiaryReferralGroupMember.objects.filter(emailuser_id=user.id).exists() or
-            ApiaryOrganisationAccessGroupMember.objects.filter(emailuser_id=user.id).exists()
+    return (
+        user
+        and hasattr(user, "id")
+        and (
+            user.is_superuser
+            or belongs_to(user, settings.APIARY_ADMIN_GROUP)
+            or belongs_to(user, settings.APPROVED_APIARY_EXTERNAL_USERS_GROUP)
+            or ApiaryApproverGroupMember.objects.filter(emailuser_id=user.id).exists()
+            or ApiaryAssessorGroupMember.objects.filter(emailuser_id=user.id).exists()
+            or ApiaryReferralGroupMember.objects.filter(emailuser_id=user.id).exists()
+            or ApiaryOrganisationAccessGroupMember.objects.filter(emailuser_id=user.id).exists()
         )
     )
+
+
+@lru_cache(maxsize=1024)
+def get_cached_ledger_organisation(organisation_id):
+    """
+    This is safe because it's keyed by an integer.
+    Multiple model instances calling this with ID 50
+    will only trigger one network call.
+    """
+    return get_organisation(organisation_id).get("data")
